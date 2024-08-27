4dc3f53c88ae260afed0a59c1bffd6f3c1336e0d
Thiago Felinto <thiagofelintocd@gmail.com>
2024-08-26 23:25:01 -0300

    Add UserResource e UserServices
    Adcionando o UserResource e UserServices "funcionais"

--- src/main/java/br/com/thiagofelinto/medicalconsult/user/resource/UserResource.java
+++ src/main/java/br/com/thiagofelinto/medicalconsult/user/resource/UserResource.java
@@ -1,4 +1,12 @@
 package br.com.thiagofelinto.medicalconsult.user.resource;
 
+import br.com.thiagofelinto.medicalconsult.user.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class UserResource {
+    @Autowired
+    private UserServices service;
 }
--- src/main/java/br/com/thiagofelinto/medicalconsult/user/services/UserServices.java
+++ src/main/java/br/com/thiagofelinto/medicalconsult/user/services/UserServices.java
@@ -1,4 +1,14 @@
 package br.com.thiagofelinto.medicalconsult.user.services;
 
+import br.com.thiagofelinto.medicalconsult.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class UserServices {
+
+    @Autowired
+    private UserRepository userRepository;
+
 }
