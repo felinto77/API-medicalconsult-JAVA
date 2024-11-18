7be215267d31a5d2bc9e38f74f4f1b9a3c8015f2
Thiago Felinto <thiagofelintocd@gmail.com>
2024-11-18 20:27:38 -0300

    ATT no SecurityConfig
    Alterações no SecurityConfig
    ADD as perms, e as senhas para as permissões

--- src/main/java/br/com/thiagofelinto/medicalconsult/config/SecurityConfig.java
+++ src/main/java/br/com/thiagofelinto/medicalconsult/config/SecurityConfig.java
@@ -1,11 +1,47 @@
 package br.com.thiagofelinto.medicalconsult.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@Enablewebsecurity
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/user").hasAnyRole("ADMIN", "SECRETARIO")
+                        .requestMatchers(HttpMethod.GET, "/user").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/user").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/user").hasAnyRole("ADMIN", "SECRETARIO")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("THIAGO").password(passwordEncoder.encode("12345")).roles("ADMIN")
+                .and()
+                .withUser("THIGGA").password(passwordEncoder.encode("98765")).roles("PACIENTE")
+                .and()
+                .withUser("JULIANA").password(passwordEncoder.encode("121212")).roles("SECRETARIA");
+
+    }
 
     private static final String[] PUBLIC_MATCHES = {
             "/h2-console/**",
